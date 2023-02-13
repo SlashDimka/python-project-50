45967b0089f9c21d210793f99c41107e27eea56f
SlashDimka <korolkovpro@yandex.ru>
2023-02-14 00:19:48 +0600

    step obnova

--- gendiff/parse.py
+++
@@ -1,9 +0,0 @@
-import json
-import yaml
-
-
-def open_file(file):
-    if file.endswith('json'):
-        return json.load(open(file))
-    else:
-        return yaml.load(open(file), Loader=yaml.Loader)
