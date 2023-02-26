58420a416f33eabd94531f90995bb601bda54f45
SlashDimka <korolkovpro@yandex.ru>
2023-02-26 10:02:01 +0600

    refresh

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -1,11 +1,31 @@
-import yaml
 import json
+import yaml
+from pathlib import Path
 
 
-def parse_data(data, format_):
-    """Parse file. Supported types is JSON, YAML and YML."""
-    if format_ == 'json':
-        return json.loads(data)
-    elif format_ == 'yaml' or format_ == 'yml':
-        return yaml.safe_load(data)
+def get_file_data(path_to_file):
+    format = get_extention(path_to_file)
+    file_data = load_file_data(open_file(path_to_file), format)
+    return file_data
+
+
+def get_extention(path_to_file):
+    extention = Path(path_to_file).suffix
+    if extention.lower() == '.json':
+        return 'json'
+    elif extention == '.yaml':
+        return 'yaml'
+    elif extention == '.yml':
+        return 'yml'
+
+
+def open_file(filename):
+    return open(filename)
+
+
+def load_file_data(filename, format):
+    if format == 'json':
+        return json.load(filename)
+    elif format == 'yaml' or format == 'yml':
+        return yaml.safe_load(filename)
     raise TypeError("Can't read because input data type is not supported")
