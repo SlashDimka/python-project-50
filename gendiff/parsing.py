1aa6e8020127424ece509b32657744ae3e7f5ab9
SlashDimka <korolkovpro@yandex.ru>
2023-02-26 14:19:00 +0600

    refresh

--- gendiff/parsing.py
+++ gendiff/parsing.py
@@ -3,6 +3,13 @@ import json
 import yaml
 import os
 
+def parse(data, format_):
+    """Parses data"""
+    if format_ == 'json':
+        return json.loads(data)
+    if format_ == 'yaml':
+        return yaml.safe_load(data)
+
 def read_file(filepath):
     """Reads data from a file"""
     with open(filepath, 'r') as file:
@@ -16,10 +23,3 @@ def get_format(filepath):
     elif ext == '.json':
         return 'json'
     raise TypeError("Can't read because input data type is not supported")
-
-def parse(data, format_):
-    """Parses data"""
-    if format_ == 'json':
-        return json.loads(data)
-    if format_ == 'yaml':
-        return yaml.safe_load(data)
